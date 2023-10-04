bf1f390a70a0ef6fb8ff50ab463f74cf093234ff
Adedoyin Kemi <kemidoy@gmail.com>
2023-10-04 13:09:51 +0100

    Create a bug model and a bug

--- bug/polls/models.py
+++ bug/polls/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Bug(models.Model):
+    BUG_TYPES = (
+        ('error', 'Error'),
+        ('new_feature', 'New Feature'),
+        ('enhancement', 'Enhancement'),
+        ('security', 'Security'),
+        ('other', 'Other'),
+    )
+
+    STATUS_CHOICES = (
+        ('to_do', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+        ('review', 'Review'),
+        ('testing', 'Testing'),
+    )
+
+    description = models.TextField()
+    bug_type = models.CharField(max_length=20, choices=BUG_TYPES, default='error')
+    report_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='to_do')
+
+    def __str__(self):
+        return self.description
