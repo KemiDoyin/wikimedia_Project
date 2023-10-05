9cfc1cb7d624d4aae8ddfc8a606437fafd80a58a
Adedoyin Kemi <kemidoy@gmail.com>
2023-10-05 11:52:07 +0100

    Views and Templates

--- bug/polls/urls.py
+++ bug/polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('register_bug/', views.register_bug, name='register_bug'),
+    path('bug/<int:bug_id>/', views.view_bug, name='view_bug'),
+    path('bug_list/', views.bug_list, name='bug_list'),
 ]
--- bug/polls/views.py
+++ bug/polls/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Bug
+from .forms import BugForm
 
-# Create your views here.
-from django.http import HttpResponse
+def register_bug(request):
+    if request.method == 'POST':
+        form = BugForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bug_list')
+    else:
+        form = BugForm()
+    return render(request, 'register_bug.html', {'form': form})
+
+def view_bug(request, bug_id):
+    bug = Bug.objects.get(pk=bug_id)
+    return render(request, 'view_bug.html', {'bug': bug})
+
+def bug_list(request):
+    bugs = Bug.objects.all()
+    return render(request, 'bug_list.html', {'bugs': bugs})
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
